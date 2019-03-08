f51d7902163f10ec87a8cbfa8deea8afd59d98d3
Toon <toonschaap3@gmail.com>
2019-03-08 08:18:44 +0100

    First sprint commit
    Commit for first merge

--- MythGame/Assets/Toon/Scripts/Testmove.cs
+++ MythGame/Assets/Toon/Scripts/Testmove.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OpenChest : MonoBehaviour
+public class Testmove : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W)){
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
     }
 }
