8caa8523b54d9f09cf1f09d2275d8bdd72d0b792
Lucas4185 <30445090+Lucas4185@users.noreply.github.com>
2019-03-14 09:06:19 +0100

    Enemy Health
    Enemy health and death script

---
+++ MythGame/Assets/Enemy/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    public int HealthCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(HealthCounter == 0)
+        {
+            StartCoroutine("EnemyDeathTimer");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Weapon")
+        {
+            HealthCounter--;
+            Debug.Log("Enemy hit");
+        }
+       
+    }
+
+    IEnumerator EnemyDeathTimer()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(this.gameObject);
+        
+    }
+}
