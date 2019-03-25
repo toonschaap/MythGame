81e694027156e81153230b372bd0501a23800e74
Lucas4185 <30445090+Lucas4185@users.noreply.github.com>
2019-03-25 10:25:17 +0100

    Anasi rock fall attack

---
+++ MythGame/Assets/Scripts/Boss/BossRockAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossRockAttack : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Rock;
+   
+    [SerializeField]
+    private float spawnThreshold = 100f;
+    private float spawnTimer;
+
+
+
+    private void Update()
+    {
+   
+        spawnTimer += 0.005f;
+        if (spawnTimer >= spawnThreshold)
+        {
+            RockAttack();
+            spawnTimer = 0;
+        }
+
+
+    }
+
+    private void RockAttack()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(-4.0f, 3.5f), 15, -8);
+        Instantiate(Rock, spawnPosition, Quaternion.identity);
+        
+    }
+
+}
