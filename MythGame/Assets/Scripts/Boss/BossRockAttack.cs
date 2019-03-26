bde707a1d6366ad5f751cca71022bd7f0250cb0d
Lucas4185 <30445090+Lucas4185@users.noreply.github.com>
2019-03-26 13:58:55 +0100

    Updated boss attack

--- MythGame/Assets/Scripts/Boss/BossRockAttack.cs
+++ MythGame/Assets/Scripts/Boss/BossRockAttack.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,28 +12,48 @@ public class BossRockAttack : MonoBehaviour
     private float spawnThreshold = 100f;
     private float spawnTimer;
 
+    private bool SlamAttack = false;
+
 
 
     private void Update()
     {
-   
-        spawnTimer += 0.005f;
-        if (spawnTimer >= spawnThreshold)
-        {
-            RockAttack();
-            spawnTimer = 0;
-        }
 
+        // START SLAM ANIMATION HERE
+        StartCoroutine("StartSlamAnimation");
+        spawnTimer += 0.01f;
+        if (SlamAttack == true)
+        {
+            if (spawnTimer >= spawnThreshold)
+            {
+                RockAttack();
+                spawnTimer = 0;
+            }
+        }
 
     }
 
     private void RockAttack()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(-4.0f, 3.5f), 15, -8);
-        Instantiate(Rock, spawnPosition, Quaternion.identity);
-        
+        Vector3 spawnPosition = new Vector3(UnityEngine.Random.Range(-4.0f, 3.5f), 15, -8);
+        Instantiate(Rock, spawnPosition, Quaternion.identity);     
+        StopCoroutine("StartSlamAnimation");
+        StartCoroutine("StartSlam");
+   
+
     }
 
-   
+    IEnumerator StartSlamAnimation()
+    {
+        yield return new WaitForSeconds(5);
+        SlamAttack = true;     
+    }
+
+    IEnumerator StartSlam()
+    {
+        yield return new WaitForSeconds(5);
+        SlamAttack = false;
+    }
+
 
 }
