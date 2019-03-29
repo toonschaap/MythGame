05385fc85e218d88c1e023eff290489ab01d6f7c
Lucas4185 <30445090+Lucas4185@users.noreply.github.com>
2019-03-29 11:38:03 +0100

    Mainmenu added

--- MythGame/Assets/Scripts/Menu/Menu.cs
+++ MythGame/Assets/Scripts/Menu/Menu.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -52,7 +53,7 @@ public class Menu : MonoBehaviour
 
     void StopOnClick()
     {
-        UnityEditor.EditorApplication.isPlaying = false;
+        SceneManager.LoadScene("MainMenu");
     }
 
 }
