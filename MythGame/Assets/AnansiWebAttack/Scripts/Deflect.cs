cb206cf73887586366a7b4366eb718b7dfb8b451
Toon <30626397+toonschaap@users.noreply.github.com>
2019-03-29 11:38:41 +0100

    Problems with web deflect

---
+++ MythGame/Assets/AnansiWebAttack/Scripts/Deflect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deflect : MonoBehaviour
+{
+    
+    
+    void Update()
+    {
+
+
+    }
+
+    public void OnTriggerEnter(Collider collision)
+    {
+
+        if (collision.gameObject.tag == "projectile")
+        {
+            GameObject finder = GameObject.FindGameObjectWithTag("projectile");
+            finder.GetComponent<WebBallMovement>().InverseMovement();
+            Debug.Log("collide");
+        }
+    }
+}
