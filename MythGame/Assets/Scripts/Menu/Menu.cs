0fa499071fa13afbd9cc858878fc389cc2cd7844
Lucas4185 <30445090+Lucas4185@users.noreply.github.com>
2019-03-29 09:36:42 +0100

    Menu Continue and Stop

--- MythGame/Assets/Scripts/Menu/Menu.cs
+++ MythGame/Assets/Scripts/Menu/Menu.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+   [SerializeField]
+   private GameObject MenuList;
+
+   [SerializeField]
+   private Button ContinueButton, SettingsButton, StopButton;
+
+    private int menuInt; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ContinueButton.onClick.AddListener(ContinueOnClick);
+        SettingsButton.onClick.AddListener(SettingsOnClick);
+        StopButton.onClick.AddListener(StopOnClick);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            MenuList.SetActive(true);
+            menuInt++;
+            Time.timeScale = 0;
+        }
+        if (menuInt == 2)
+        {
+            MenuList.SetActive(false);
+            menuInt = 0;
+            Time.timeScale = 1;
+        }
     }
+
+    void ContinueOnClick()
+    {
+        MenuList.SetActive(false);
+        menuInt = 0;
+        Time.timeScale = 1;
+    }
+
+    void SettingsOnClick()
+    {
+        Debug.Log("Settings");
+    }
+
+    void StopOnClick()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
 }
