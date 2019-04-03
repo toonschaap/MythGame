286f21043afde7b14eaa617c26af6f9e79d4a9b1
Lucas4185 <30445090+Lucas4185@users.noreply.github.com>
2019-04-03 16:00:39 +0200

    Updated Slam attack

--- MythGame/Assets/MartijnWorpskpace/Anansi/AnansiAttacks/SlamAttack/Scripts/Boss/DestroyRocks.cs
+++ MythGame/Assets/MartijnWorpskpace/Anansi/AnansiAttacks/SlamAttack/Scripts/Boss/DestroyRocks.cs
@@ -22,10 +22,7 @@ public class DestroyRocks : MonoBehaviour
         { 
             Destroy(other.gameObject);
         }
-        if (other.gameObject.tag == "Player" && this.gameObject.tag == "Rock")
-        {
-            Destroy(other.gameObject);
-        }
+        
     }
    
 }
