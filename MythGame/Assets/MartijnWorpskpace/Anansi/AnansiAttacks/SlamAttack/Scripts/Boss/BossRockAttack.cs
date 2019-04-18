32e7cc565e41a6ecff0c11b9ed318644d3e2eec8
Lucas4185 <30445090+Lucas4185@users.noreply.github.com>
2019-04-18 09:25:42 +0200

    Code polished

--- MythGame/Assets/MartijnWorpskpace/Anansi/AnansiAttacks/SlamAttack/Scripts/Boss/BossRockAttack.cs
+++ MythGame/Assets/MartijnWorpskpace/Anansi/AnansiAttacks/SlamAttack/Scripts/Boss/BossRockAttack.cs
@@ -14,30 +14,69 @@ public class BossRockAttack : MonoBehaviour
     [SerializeField]
     private float spawnThreshold = 100f;
     private float spawnTimer;
+    private bool RockAnimAttack;
 
-    private bool SlamAttack = false;
+    [HideInInspector]
+    public Animator anim;
 
-  
+    public GameObject activeModel;
+
+    private bool RockAttack = false;
+
+    public AudioSource SlamSound;
+
+
+
+    private void Start()
+    {
+        SetupAnimator();
+        StartCoroutine("StartSlamAnimation");
+    }
+
+
+    private void SetupAnimator()
+    {
+        if (activeModel == null)
+        {
+            anim = activeModel.GetComponent<Animator>();
+            if (anim == null)
+            {
+                Debug.Log("no model found");
+            }
+            else
+            {
+                activeModel = anim.gameObject;
+            }
+        }
+
+        anim = activeModel.GetComponent<Animator>();
+    }
+    private void HandleMovementAnimations()
+    {
+        anim.SetBool("RockAttack", RockAnimAttack);
+    }
+
+
+
+
 
     private void Update()
     {
+        HandleMovementAnimations();
 
-        // START SLAM ANIMATION HERE
-
-        StartCoroutine("StartSlamAnimation");
         spawnTimer += 0.01f;
-        if (SlamAttack == true)
+        if (RockAttack)
         {
             if (spawnTimer >= spawnThreshold)
             {
-                RockAttack();
+                AnansiRockAttack();
                 spawnTimer = 0;
             }
         }
 
     }
 
-    private void RockAttack()
+    private void AnansiRockAttack()
     {
         Vector3 playerPos = player.transform.position;
         Vector3 playerDirection = player.transform.up;
@@ -45,23 +84,29 @@ public class BossRockAttack : MonoBehaviour
 
         Vector3 spawnPos = playerPos + playerDirection * height;
 
-        Instantiate(Rock, spawnPos, Quaternion.identity);     
-        StopCoroutine("StartSlamAnimation");
+        Instantiate(Rock, spawnPos, Quaternion.identity);
+        //StopCoroutine("StartSlamAnimation");
         StartCoroutine("StartSlam");
-   
+
 
     }
 
     IEnumerator StartSlamAnimation()
     {
+
         yield return new WaitForSeconds(5);
-        SlamAttack = true;     
+        RockAnimAttack = true;
+        RockAttack = true;
+        StartCoroutine("StartSlamAnimation");
     }
 
     IEnumerator StartSlam()
     {
+
         yield return new WaitForSeconds(5);
-        SlamAttack = false;
+        RockAnimAttack = false;
+        RockAttack = false;
+        
     }
 
 
